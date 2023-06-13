a6939401a3f3d554b28426d626ce1b827ddfcd06
Ri2023 <belvee@yahoo.com>
2023-06-13 07:32:25 -0700

    append-write

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-"""Defines a file-appending function."""
+"""
+read_lines function module.
+
+Define read_lines function.
+"""
 
 
-def append_write(filename="", text=""):
-    """Appends a string to the end of a UTF8 text file.
-    Args:
-        filename (str): The name of the file to append to.
-        text (str): The string to append to the file.
-    Returns:
-        The number of characters appended.
+def read_lines(filename="", nb_lines=0):
+    """Prints the number of lines specified of a text file (UTF8).
+    filename (str): the file, must exist, must have permissions.
+    nb_lines (int), default=0: the number of lines to print.
     """
-    with open(filename, "a", encoding="utf-8") as f:
-        return f.write(text)
+    with open(filename, encoding="UTF-8") as myfile:
+        count = 0
+        for line in myfile:
+            if nb_lines <= 0 or count < nb_lines:
+                print(line, end="")
+            count += 1
 
